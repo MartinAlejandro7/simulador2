a9072dec15c7d51c70ff59dedec3a18116b51557
MartinAlejandro7 <alejomartin7532@gmail.com>
2026-07-16 15:12:00 -0500

    funcionalidad de registro de clientes

--- simulador_completo.js
+++ simulador_completo.js
@@ -31,3 +31,39 @@ function guardarTasa(){
     cmpMensaje.innerHTML=("La tasa debe estar entre 10% y 20%")
   }
 }
+
+function guardarCliente(){
+  let caja1Cedula =recuperaraTexto("txtCedula");
+  let caja2Nombre =recuperaraTexto("textNombre");
+  let caja3Apellido =recuperaraTexto("textApellido");
+  let numIngresos = recuperarInt("numIngresos");
+  let numEgresos = recuperarInt("numEgresos");
+  let cliente = {}
+
+  cliente.cedula = caja1Cedula;
+  cliente.nombre = caja2Nombre;
+  cliente.apellido = caja3Apellido;
+  cliente.ingresos = numIngresos;
+  cliente.egresos = numEgresos;
+
+  clientes.push(cliente)
+
+  pintarClientes()
+}
+
+function pintarClientes(){
+  let cmpClientes = document.getElementById("tablaClientes");
+  let contenido = "";
+  let infoCliente;
+  
+  for(let i=0; i<clientes.length; i++){
+    infoCliente = clientes[i];
+    contenido = contenido +"<tr>"+
+    "<td>"+infoCliente.cedula+"</td>" +
+    "<td>"+infoCliente.nombre+"</td>"+"<td>"+infoCliente.apellido+"</td>"+
+    "<td>"+infoCliente.ingresos+"</td>"+"<td>"+infoCliente.egresos+"</td>"+
+    "<td><button>Actualizar</button</td>"+
+    "</tr>"
+  }
+  cmpClientes.innerHTML = contenido
+}
